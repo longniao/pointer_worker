d785c30fe6eaf50129b9e89d398dec7fc3c1f1ad
longniao <longniao@gmail.com>
2019-08-29 19:36:19 +0800

    app

---
+++ app/__init__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from config import config
+from flask import Flask
+from flask_apscheduler import APScheduler
+from flask_babel import lazy_gettext as _
+from flask_login import LoginManager
+from flask_mongoengine import MongoEngine
+from flask_wtf.csrf import CSRFProtect
+
+# 全局变量
+db = MongoEngine()
+csrf = CSRFProtect()
+scheduler = APScheduler()
+
+# Set up Flask-Login
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+login_manager.login_message = _('Please log in to access this page.')
+
+def create_app(config_name):
+    '''
+    创建APP
+    :param config_name:
+    :return:
+    '''
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    # Set up extensions
+    db.init_app(app)
+    login_manager.init_app(app)
+    csrf.init_app(app)
+    scheduler.init_app(app)
+
+    return app
--- config.py
+++ config.py
@@ -13,5 +13,78 @@ class Config:
     '''
     app settings
     '''
-    mongodb = parser.config['mongodb']
-    proxy   = parser.config['proxy']
+    DEBUG           = parser.config['app']['debug']
+    TESTING         = parser.config['app']['testing']
+    BCRYPT_LEVEL    = parser.config['app']['bcrypt_level']
+    APP_NAME        = parser.config['app']['app_name']
+    SECRET_KEY      = parser.config['app']['secret_key']
+    WTF_CSRF_ENABLED    = parser.config['app']['wtf_csrf_enabled']
+
+    # mongodb settings
+    MONGODB_SETTINGS = parser.config['mongodb']['mongodb_settings']
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    ASSETS_DEBUG = True
+
+    @classmethod
+    def init_app(cls, app):
+        print('THIS APP IS IN DEBUG MODE. YOU SHOULD NOT SEE THIS IN PRODUCTION.')
+
+
+class TestingConfig(Config):
+    TESTING = True
+    WTF_CSRF_ENABLED = False
+
+    @classmethod
+    def init_app(cls, app):
+        print('THIS APP IS IN TESTING MODE.  YOU SHOULD NOT SEE THIS IN PRODUCTION.')
+
+
+class ProductionConfig(Config):
+    SSL_DISABLE = True
+
+    @classmethod
+    def init_app(cls, app):
+        Config.init_app(app)
+        assert Config.SECRET_KEY, 'SECRET_KEY IS NOT SET!'
+
+
+class HerokuConfig(ProductionConfig):
+    @classmethod
+    def init_app(cls, app):
+        ProductionConfig.init_app(app)
+
+        # Handle proxy server headers
+        from werkzeug.contrib.fixers import ProxyFix
+        app.wsgi_app = ProxyFix(app.wsgi_app)
+
+
+class UnixConfig(ProductionConfig):
+    @classmethod
+    def init_app(cls, app):
+        ProductionConfig.init_app(app)
+
+        # Log to syslog
+        import logging
+        from logging.handlers import SysLogHandler
+        syslog_handler = SysLogHandler()
+        syslog_handler.setLevel(logging.WARNING)
+        app.logger.addHandler(syslog_handler)
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig,
+    'heroku': HerokuConfig,
+    'unix': UnixConfig
+}
+
+config_env = os.getenv('CONFIG_ENV', 'default')
