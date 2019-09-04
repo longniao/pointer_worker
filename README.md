8dd2516dfa96ed7cf36e87c5ee9e37c5c65de17a
longniao <longniao@gmail.com>
2019-09-04 16:59:30 +0800

    indicators

---
+++ README.md
@@ -0,0 +1,7 @@
+# pointer_worker
+
+```
+# https://github.com/mrjbq7/ta-lib
+brew install ta-lib
+pip install TA-Lib
+```
---
+++ app/indicators/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+'''
+https://github.com/haibeicode/indicator/tree/master/indicator
+pip install indicator
+
+'''
