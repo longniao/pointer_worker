4c238d78658ac7cd5bd47275e1333ef5d1392646
longniao <longniao@gmail.com>
2019-09-05 21:58:16 +0800

    feixiaohao

--- app/indicators/technology/trend.py
+++ app/indicators/technology/trend.py
@@ -436,26 +436,15 @@ def JLHB(df, N=7, M=5):
     # })
 
 
-def TD(df, sequence=13, count=None):
+def TD(df, sequence=13):
     """
-    TD序列指标:TD计数
-
-    TD结构: 连续9根K线的收盘价比各自前面第4根K线的收盘价 高或低。
+    TD序列指标: 连续9根K线的收盘价比各自前面第4根K线的收盘价 高或低。
     TD计数: 连续出现9根K线（TD结构，9根K线的收盘价都比各自前面的第4根K线收盘价高或低）之后，进行TD计数，最多计到13。
-    TD严格计数：从第9到第13根K线，要求比各自前第2根K的最高价高 或 最低价低。
-    TD宽松计数：从第9到第13根K线，要求比各自前第2根K的收盘价 高或低。
-
-    TD序列: TD结构 + TD计数 共同构成 TD序列.
-    TD组合: 比TD序列更严格的是TD组合。
-    对于日K线图来说，德马克认为TD组合要比TD系列更有效一些。
-    最有效的TD信号: 理想状态是， TD系列和TD组合信号发生重合，这样的信号更有效。
-    TD最有效的交易信号：TD计数与TD组合发生共震时! 不同时间周期TD计数或TD组合 发生共震时!
-
     :param df:
     :param sequence: 序列长度
-    :param count: strict 严格计数 | loose 宽松计数
     :return:
     """
+    df = df.copy()
     condv = (df['volume'] > 0)
     cond1 = (df['close'] > df['close'].shift(4))
     cond2 = (df['close'] < df['close'].shift(4))
@@ -485,24 +474,44 @@ def TD(df, sequence=13, count=None):
     df['tdc'] = df.apply((lambda x: x['tdb_b'] % sequence if x['tdb_b'] > sequence else x['tdc']), axis=1)
     df['tdc'] = df.apply((lambda x: (x['tds_b'] % sequence) * -1 if x['tds_b'] > sequence else x['tdc']), axis=1)
 
-    # TD计数
-    if not count:
-        con_high = con_low = None
-
-        # 严格计数
-        if count == 'strict':
-            con_high = (df['high'] > df['high'].shift(2))
-            con_low = (df['low'] < df['low'].shift(2))
-        # 宽松计数
-        elif count == 'loose':
-            con_high = (df['close'] > df['close'].shift(2))
-            con_low = (df['close'] < df['close'].shift(2))
-
-        if con_high and con_low:
-            c_high = df.groupby((((con_high)[condv])).cumsum()).cumcount()
-            c_low = df.groupby((((con_low)[condv])).cumsum()).cumcount()
-
     return pd.DataFrame(index=df.index, data={
         'TD_count': df['tdc']
     })
 
+
+def TD_COUNT(df, shift=2, column='close'):
+    """
+    TD计数: strict 严格计数 | loose 宽松计数
+    TD计数: 连续出现9根K线（TD结构，9根K线的收盘价都比各自前面的第4根K线收盘价高或低）之后，进行TD计数，最多计到13。
+    TD严格计数：从第9到第13根K线，要求比各自前第2根K的最高价高 或 最低价低。
+    TD宽松计数：从第9到第13根K线，要求比各自前第2根K的收盘价 高或低。
+
+    TD序列: TD结构 + TD计数 共同构成 TD序列.
+    TD组合: 比TD序列更严格的是TD组合。
+    对于日K线图来说，德马克认为TD组合要比TD系列更有效一些。
+    最有效的TD信号: 理想状态是， TD系列和TD组合信号发生重合，这样的信号更有效。
+    TD最有效的交易信号：TD计数与TD组合发生共震时! 不同时间周期TD计数或TD组合 发生共震时!
+    :param df:
+    :param shift: 偏移长度
+    :return:
+    """
+    df = df.copy()
+    condv = (df['vol'] > 0)
+    cond1 = (df[column] > df[column].shift(shift))
+    cond2 = (df[column] < df[column].shift(shift))
+
+    df['cond_tdb_a'] = ((cond1)[condv]).cumsum()
+    df['cond_tds_a'] = ((cond2)[condv]).cumsum()
+
+    df['tdb_a'] = df.groupby(
+        df['cond_tdb_a']
+    ).cumcount()
+    df['tds_a'] = df.groupby(
+        df['cond_tds_a']
+    ).cumcount()
+    df['tdc'] = df['tds_a'] - df['tdb_a']
+
+    key = 'tdc_%s' % column
+    return pd.DataFrame(index=df.index, data={
+        key: df['tdc']
+    })
--- tests/test_td.py
+++ tests/test_td.py
@@ -19,60 +19,17 @@ df = pro.coinbar(exchange='huobi', symbol='btcusdt', freq='daily', start_date='2
 
 df.sort_values(by="date", inplace=True)
 
-print(df)
-
-def TD9(df):
-    """
-    TD序列指标
-    :param df:
-    :return:
-    """
-    condv = (df['vol'] > 0)
-    cond1 = (df['close'] > df['close'].shift(4))
-    cond2 = (df['close'] < df['close'].shift(4))
-
-    df['cond_tdb_a'] = (df.groupby((((cond1)[condv])).cumsum()).cumcount() % 13 == 0).cumsum()
-    df['cond_tds_a'] = (df.groupby((((cond2)[condv])).cumsum()).cumcount() % 13 == 0).cumsum()
-
-    df['cond_tdb_b'] = (df.groupby((((cond1)[condv])).cumsum()).cumcount() % 13 != 0).cumsum()
-    df['cond_tds_b'] = (df.groupby((((cond2)[condv])).cumsum()).cumcount() % 13 != 0).cumsum()
-
-    df['tdb_a'] = df.groupby(
-        df['cond_tdb_a']
-    ).cumcount()
-
-    df['tds_a'] = df.groupby(
-        df['cond_tds_a']
-    ).cumcount()
-
-    df['tdb_b'] = df.groupby(
-        df['cond_tdb_b']
-    ).cumcount()
-
-    df['tds_b'] = df.groupby(
-        df['cond_tds_b']
-    ).cumcount()
-
-    df['tdc'] = df['tds_a'] - df['tdb_a']
-    df['tdc'] = df.apply((lambda x: x['tdb_b'] % 13 if x['tdb_b'] > 13 else x['tdc']), axis=1)
-    df['tdc'] = df.apply((lambda x: (x['tds_b'] % 13) * -1 if x['tds_b'] > 13 else x['tdc']), axis=1)
-
-    return pd.DataFrame(index=df.index, data={
-        'date': df['date'],
-        'open': df['open'],
-        'high': df['high'],
-        'low': df['low'],
-        'close': df['close'],
-        'td_count': df['tdc']
-    })
 
 
 def TD(df, sequence=13):
     """
-    TD序列指标
+    TD序列指标: 连续9根K线的收盘价比各自前面第4根K线的收盘价 高或低。
+    TD计数: 连续出现9根K线（TD结构，9根K线的收盘价都比各自前面的第4根K线收盘价高或低）之后，进行TD计数，最多计到13。
     :param df:
+    :param sequence: 序列长度
     :return:
     """
+    df = df.copy()
     condv = (df['vol'] > 0)
     cond1 = (df['close'] > df['close'].shift(4))
     cond2 = (df['close'] < df['close'].shift(4))
@@ -103,21 +60,54 @@ def TD(df, sequence=13):
     df['tdc'] = df.apply((lambda x: (x['tds_b'] % sequence) * -1 if x['tds_b'] > sequence else x['tdc']), axis=1)
 
     return pd.DataFrame(index=df.index, data={
-        'date': df['date'],
-        'open': df['open'],
-        'high': df['high'],
-        'low': df['low'],
-        'close': df['close'],
         'td_count': df['tdc']
     })
 
+def TD_COUNT(df, shift=2, column='close'):
+    """
+    TD计数: strict 严格计数 | loose 宽松计数
+    TD计数: 连续出现9根K线（TD结构，9根K线的收盘价都比各自前面的第4根K线收盘价高或低）之后，进行TD计数，最多计到13。
+    TD严格计数：从第9到第13根K线，要求比各自前第2根K的最高价高 或 最低价低。
+    TD宽松计数：从第9到第13根K线，要求比各自前第2根K的收盘价 高或低。
 
-td_df = TD(df)
+    TD序列: TD结构 + TD计数 共同构成 TD序列.
+    TD组合: 比TD序列更严格的是TD组合。
+    对于日K线图来说，德马克认为TD组合要比TD系列更有效一些。
+    最有效的TD信号: 理想状态是， TD系列和TD组合信号发生重合，这样的信号更有效。
+    TD最有效的交易信号：TD计数与TD组合发生共震时! 不同时间周期TD计数或TD组合 发生共震时!
+    :param df:
+    :param shift: 偏移长度
+    :return:
+    """
+    condv = (df['vol'] > 0)
+    cond1 = (df[column] > df[column].shift(shift))
+    cond2 = (df[column] < df[column].shift(shift))
 
-cols=['date','open','high','low','close','td_count']
-td_df=td_df.ix[:,cols]
+    df['cond_tdb_a'] = ((cond1)[condv]).cumsum()
+    df['cond_tds_a'] = ((cond2)[condv]).cumsum()
 
-print(td_df)
+    df['tdb_a'] = df.groupby(
+        df['cond_tdb_a']
+    ).cumcount()
+    df['tds_a'] = df.groupby(
+        df['cond_tds_a']
+    ).cumcount()
+    df['tdc'] = df['tds_a'] - df['tdb_a']
+
+    key = 'tdc_%s' % column
+    return pd.DataFrame(index=df.index, data={
+        key: df['tdc']
+    })
+
+df_td = df.copy()
+df_td['td_count'] = TD(df)
+df_td['td_close'] = TD_COUNT(df, column='close')
+df_td['td_high'] = TD_COUNT(df, column='high')
+df_td['td_low'] = TD_COUNT(df, column='low')
+
+print(df_td)
+
+
 
 if __name__ == '__main__':
     try:
